b78642f633b8f306dd71ca3745c3454cedbd1745
Luka <vujnovacluka89@gmail.com>
2021-10-10 20:12:39 +0200

    Initial Commit

---
+++ WidgetApp/WidgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetAppApp.swift
+//  WidgetApp
+//
+//  Created by Luka Vujnovac on 10.10.2021..
+//
+
+import SwiftUI
+
+@main
+struct WidgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
